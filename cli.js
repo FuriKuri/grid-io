41cdf6f4fb1cf7e709d8bf1e7311d7727d51592c
Theo Pack <tf.pack@gmail.com>
2015-03-04 15:02:05 +0100

    Add client

--- cli.js
+++ cli.js
@@ -2,8 +2,8 @@
 
 var Master = require('./lib/master');
 
-var Node = require('./lib/node');
+var Node = require('./lib/client');
 
 new Master(1337).listen();
 
-new Node(1338).listen();
+new Client(1338).listen();
