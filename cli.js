ff4e085894492c56552def797ab24a6f536b850d
Theo Pack <tf.pack@gmail.com>
2015-03-08 16:13:27 +0100

    Add notify listener

--- cli.js
+++ cli.js
@@ -9,3 +9,7 @@ new Master(1337).listen();
 var client = new Client(1338);
 client.listen();
 client.connect('127.0.0.1', 1337);
+
+var client2 = new Client(1339);
+client2.listen();
+client2.connect('127.0.0.1', 1337);
